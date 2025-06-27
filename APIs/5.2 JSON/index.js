caa827b7c4a28387e0d91646c5134a30ebed4222
userzenox <piyush.132kp@gmail.com>
2025-06-27 17:15:37 +0530

    commit-project-2

---
+++ APIs/5.2 JSON/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+//Step 1: Run the solution.js file without looking at the code.
+//Step 2: You can go to the recipe.json file to see the full structure of the recipeJSON below.
+const recipeJSON =
+  '[{"id": "0001","type": "taco","name": "Chicken Taco","price": 2.99,"ingredients": {"protein": {"name": "Chicken","preparation": "Grilled"},  "salsa": {"name": "Tomato Salsa","spiciness": "Medium"},  "toppings": [{"name": "Lettuce",  "quantity": "1 cup",  "ingredients": ["Iceberg Lettuce"]  },      {"name": "Cheese",  "quantity": "1/2 cup",  "ingredients": ["Cheddar Cheese", "Monterey Jack Cheese"]  },      {"name": "Guacamole",  "quantity": "2 tablespoons",  "ingredients": ["Avocado", "Lime Juice", "Salt", "Onion", "Cilantro"]  },      {"name": "Sour Cream",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream"]  }      ]    }  },{"id": "0002","type": "taco","name": "mutton Taco","price": 3.49,"ingredients": {"protein": {"name": "mutton","preparation": "Seasoned and Grilled"},  "salsa": {"name": "Salsa Verde","spiciness": "Hot"},  "toppings": [{"name": "Onions",  "quantity": "1/4 cup",  "ingredients": ["White Onion", "Red Onion"]  },      {"name": "Cilantro",  "quantity": "2 tablespoons",  "ingredients": ["Fresh Cilantro"]  },      {"name": "Queso Fresco",  "quantity": "1/4 cup",  "ingredients": ["Queso Fresco"]  }      ]    }  },{"id": "0003","type": "taco","name": "Fish Taco","price": 4.99,"ingredients": {"protein": {"name": "Fish","preparation": "Battered and Fried"},  "salsa": {"name": "Chipotle Mayo","spiciness": "Mild"},  "toppings": [{"name": "Cabbage Slaw",  "quantity": "1 cup",  "ingredients": [    "Shredded Cabbage",    "Carrot",    "Mayonnaise",    "Lime Juice",    "Salt"          ]  },      {"name": "Pico de Gallo",  "quantity": "1/2 cup",  "ingredients": ["Tomato", "Onion", "Cilantro", "Lime Juice", "Salt"]  },      {"name": "Lime Crema",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream", "Lime Juice", "Salt"]  }      ]    }  }]';
+
+ const data = JSON.parse(recipeJSON);
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/recipe", (req, res) => {
+  
+ 
+    console.log(req.body.choice);
+   
+   
+    var num ;
+     if(req.body.choice == "chicken"){
+          num = 0;
+     }
+     else if(req.body.choice == "mutton"){
+           num =1;
+     }else{
+      num=2;
+     }
+
+    
+      
+
+        res.render("index.ejs" , { heading : req.body.choice,
+        proteinName: data[num].ingredients.protein.name ,
+        proteinPreparation: data[num].ingredients.protein.preparation ,
+        salsa : data[num].ingredients.salsa.name,
+        topping1: data[num].ingredients.toppings[0].quantity + " of " + data[num].ingredients.toppings[0].name,
+
+        topping2: data[num].ingredients.toppings[1].quantity + " of " + data[num].ingredients.toppings[1].name,
+
+        topping3: data[num].ingredients.toppings[2].quantity + " of " + data[num].ingredients.toppings[2].name,
+       
+         })
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
