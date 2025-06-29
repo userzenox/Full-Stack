e6ef46edd53e9d75ab580dbacc87c3db1d0fd2d3
userzenox <piyush.132kp@gmail.com>
2025-06-29 23:44:10 +0530

    commit-29

--- APIs/5.5 REST APIs/index.js
+++ APIs/5.5 REST APIs/index.js
@@ -21,14 +21,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { content: "Waiting for data..." });
+
 });
 
 app.post("/get-secret", async (req, res) => {
   const searchId = req.body.id;
   try {
     const result = await axios.get(API_URL + "/secrets/" + searchId, config);
+
     res.render("index.ejs", { content: JSON.stringify(result.data) });
-  } catch (error) {
+  }
+   catch (error) {
     res.render("index.ejs", { content: JSON.stringify(error.response.data) });
   }
 });
