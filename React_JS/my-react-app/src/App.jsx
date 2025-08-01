ddee8c4c31ca2868b4e6d8217b24c864c2c29705
userzenox <piyush.132kp@gmail.com>
2025-08-01 19:45:00 +0530

    commit-1

--- React_JS/my-react-app/src/App.jsx
+++ React_JS/my-react-app/src/App.jsx
@@ -1,10 +1,31 @@
 
 import './App.css'
 
+const date = new Date(19,1,1,18);
+console.log(date.getHours());
+
+ const custom = {
+         color: "red",
+         time: "Morning"
+    }
+
+if(date.getHours() < 12 && date.getHours() > 0){
+   custom.color = "red"
+    custom.time = "Morning"
+}
+else if( date.getHours() < 18 && date.getHours()>12 ){
+    custom.color = "blue"
+    custom.time = "afternoon"
+}
+else{
+      custom.color = "green"
+    custom.time = "evening"
+}
+
 const Card =() =>{
    return (
        <div>
-          <h2> Card component</h2>
+          <h2 style= {{color: custom.color}}> Good {custom.time} </h2>
        </div>
    )
 }
