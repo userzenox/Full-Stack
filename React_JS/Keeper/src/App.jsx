19370505a33e1ca5f0feb1934541c2ceb9edbe09
userzenox <piyush.132kp@gmail.com>
2025-08-02 18:46:08 +0530

    commit-2

---
+++ React_JS/Keeper/src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import './styles.css'
+import Note from './Components/Note';
+function App() {
+  return (
+    <>
+      <Header/>
+
+       <Note/>
+      
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ React_JS/Keeper/src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const date = new Date();
+
+
+function Footer() {
+  return (
+    <>
+      <footer>
+          <p> Copyright {date.getFullYear()} </p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
