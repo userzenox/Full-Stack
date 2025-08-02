663fafa4fb04e29cb9bdcf777363c8bbcc02d30f
userzenox <piyush.132kp@gmail.com>
2025-08-02 17:07:37 +0530

    commit-2

--- React_JS/my-react-app/src/App.jsx
+++ React_JS/my-react-app/src/App.jsx
@@ -2,16 +2,21 @@
 import './App.css'
 import Heading from './Components/Heading';
 import Timing from './Components/Timing';
-
-
+import pi from "./Components/Modules"
+import { doublepi,triplepi } from './Components/Modules';
+import { Fourpi } from './Components/Modules';
 const App = () =>{
     return (
       <>
          <Heading/>
-      <h2> Greeting</h2>
+      <h2> Greeting v</h2>
 
         <Timing/>
         <Timing/>
+         
+         <h3> Value of pie is {pi}</h3>
+         <p>  {doublepi()}  </p>
+          <h3>  <Fourpi/></h3>
         </>
     )
 }
---
+++ React_JS/my-react-app/src/Components/Modules.jsx
@@ -0,0 +1,21 @@
+ 
+ const pi = 3.14345;
+
+ function doublepi() {
+      return pi*2;
+ }
+
+ 
+ function triplepi( ){
+      return pi *3;
+ }
+
+ function Fourpi(){
+      return pi *4;
+ }
+
+ export default pi;
+
+ export {doublepi,triplepi};
+ 
+ export {Fourpi};
